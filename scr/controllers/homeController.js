4b371226dd501099a7adc826bdfa1f099fd8c9e1
tocode <andrey.zarev@outlook.com>
2024-02-12 00:48:30 +0200

    added routes in homecontroller

--- scr/controllers/homeController.js
+++ scr/controllers/homeController.js
@@ -1,10 +1,49 @@
 const router = require('express').Router();
 
-router.get('/', async (req, res) => {
+router.get('/', (req, res) => {
 
     res.render("home")
 })
 
+router.get("/create", (req, res) => {
+
+    res.render("create")
+
+})
 
 
+router.post('/create', async (req, res) => {
+
+
+});
+
+router.get('/details', (req, res) => {
+    res.render("details")
+});
+
+router.post("details", (req, res) => {
+
+});
+
+
+
+
+router.get('/catalog', (req, res) => {
+    res.render("catalog")
+
+});
+
+
+router.get('/search', (req, res) => {
+    res.render("search")
+});
+
+router.post('/search', (req, res) => {
+});
+
+
+router.get('/logout', (req, res) => {
+    res.clearCookie('auth');
+    res.redirect('/');
+});
 module.exports = router
