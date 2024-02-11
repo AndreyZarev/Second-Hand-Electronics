8f8cbb04bf64190ad779083a2ed53ea7179cc4ea
tocode <andrey.zarev@outlook.com>
2024-02-11 02:44:01 +0200

    created index

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const handlebars = require('express-handlebars');
+const routes = require('./routes');
+const mongoose = require('mongoose');
+
 
 app.use(express.static(path.resolve("scr/static")))
 
 app.use(express.urlencoded({ extended: false }))
 
+app.engine("hbs", handlebars.engine({
+    extname: "hbs"
+}))
+
+app.set('view engine', "hbs");
+app.set('views', path.resolve('scr/views'));
 
 
+app.use(routes)
 const port = 3000
-app.listen(port, () => console.log("Server listening on port " + port))
+
+
+mongoose.connect(`mongodb://localhost:27017/second-hand`)
+
+    .then(() => {
+        console.log("dp is connected")
+        app.listen(port, () => console.log(`Server has been sucsessfully lauched on port ${port}`))
+    }
+
+
+    ).catch(err => console.log("db error: " + err));
