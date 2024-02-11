0b88924f9f4ac46c0bceb51f5a2a7af7f1699f8e
tocode <andrey.zarev@outlook.com>
2024-02-12 00:47:35 +0200

    initiated nodejs library

--- routes.js
+++ routes.js
@@ -4,6 +4,8 @@ const router = require('express').Router();
 const homeController = require('./scr/controllers/homeController')
 const authController = require('./scr/controllers/authController')
 
+
+
 router.use(homeController)
 router.use("/auth", authController)
 module.exports = router
