d948b14f3b909b92709c35f267855b221e18201c
tocode <andrey.zarev@outlook.com>
2024-02-11 12:20:02 +0200

    added bcrypt

--- routes.js
+++ routes.js
@@ -2,7 +2,8 @@
 const router = require('express').Router();
 
 const homeController = require('./scr/controllers/homeController')
-
+const authController = require('./scr/controllers/authController')
 
 router.use(homeController)
+router.use("/auth", authController)
 module.exports = router
