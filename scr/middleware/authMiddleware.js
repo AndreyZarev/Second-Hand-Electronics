1514b4ffafa8a08e55ffa499108b7466e7e380bf
tocode <andrey.zarev@outlook.com>
2024-02-13 01:58:21 +0200

    added authMiddleware

---
+++ scr/middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+const jwt = require('../lib/jwt');
+const { SECRET } = require('../config/secret');
+exports.auth = async (req, res, next) => {
+    // works now
+
+    const token = req.cookies["auth"];
+
+    if (!token) {
+
+        return next()
+    }
+
+    try {
+        const decodedToken = await jwt.verify(token, SECRET)
+
+        req.user = decodedToken
+        res.locals.isAuthenticated = true
+
+        next()
+    } catch {
+        res.clearCookie("auth")
+        return res.redirect("/auth/login")
+
+    }
+
+};
