2dd197bf9fd66553b6233efd2b8b37edeb8d842c
tocode <andrey.zarev@outlook.com>
2024-02-18 00:43:17 +0200

    added validations

--- scr/models/Electronics.js
+++ scr/models/Electronics.js
@@ -4,43 +4,55 @@ const electronicsSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
+        minLength: [10, "Name needs to be at least 10 characters."]
 
     },
     type: {
         type: String,
         required: true,
+        minLength: [2, "Type needs to be at least 2 characters"]
 
     },
     damages: {
         type: String,
         required: true,
+        minLength: [10, "Damages needs to be at least 10 characters."]
+
 
     },
 
     image: {
         type: String,
         required: true,
+        match: /^https?:\/\//
+
 
     },
     description: {
         type: String,
         required: true,
+        minLength: [10, "Description needs to be at least 10 characters"],
+        maxLength: [200, "Description needs to be at least 200 characters"]
 
     },
     production: {
         type: String,
         required: true,
+        min: 1900,
+        max: 2023,
 
     },
 
     exploitation: {
         type: Number,
         required: true,
+        min: [0, "Exploiration needs to be a positive number."]
 
     },
     price: {
         type: Number,
         required: true,
+        min: [0, "Price needs to be a positive number"]
 
     },
     buyingList: {
