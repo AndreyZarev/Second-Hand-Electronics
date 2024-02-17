7feaa38eaaa42a6607403afe5566a95e9802c95f
tocode <andrey.zarev@outlook.com>
2024-02-17 15:42:09 +0200

    added isBouth logic

--- scr/controllers/homeController.js
+++ scr/controllers/homeController.js
@@ -52,26 +52,51 @@ router.post('/search', (req, res) => {
 router.get("/products/:productId/details", async (req, res) => {
 
     const productId = req.params.productId
-    const product = await createService.getProduct(productId).lean()
-    const isCreator = product.owner?.email === req.user.email
     let isLoggedIn;
-    if (req.user.email) { isLoggedIn = true } else { isLoggedIn = false }
+    if (req.user?.email) { isLoggedIn = true } else { isLoggedIn = false }
+
+    const product = await createService.getProduct(productId).lean()
+    const isCreator = product.owner?.email === req.user?.email
+    let isBought = await createService.isBought(req.user?._id, req.params.productId).lean()
+
+    isBought = isBought.buyingList[0] === req.user?._id
+
 
 
     // const isCreator = await createService.getCreator(req.params)
-    res.render("details", { product, isCreator, isLoggedIn })
+    res.render("details", { product, isCreator, isLoggedIn, isBought })
 })
 
 router.get('/:productId/buy', async (req, res) => {
     const userId = req.user._id
+
     try {
         await createService.buy(userId, req.params.productId)
-        res.redirect("/products/:productId/details")
+
+        res.redirect(`/products/${req.params.productId}/details`)
 
     } catch (err) {
-
+        res.redirect(`/products/${req.params.productId}/details`)
     }
 
+})
+
+router.get("/product/:productId/delete", (req, res) => {
+    createService.deleteProduct(req.params.productId)
+    res.redirect("/catalog")
+})
+
+router.get("/product/:productId/edit", async (req, res) => {
+    const productData = await createService.getProduct(req.params.productId).lean()
+
+    res.render("edit", { ...productData })
+})
+
+
+router.post("/product/:productId/edit", async (req, res) => {
+    const productChanges = req.body
+    createService.updateProduct(req.params.productId, productChanges)
+    res.redirect(`/products/${req.params.productId}/details`)
 })
 
 module.exports = router
