581cebb798d05f5d0975456352d9a3a6de7de208
tocode <andrey.zarev@outlook.com>
2024-02-18 00:43:45 +0200

    added alternative validation

--- scr/models/User.js
+++ scr/models/User.js
@@ -11,23 +11,28 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
+        minLength: [10, "The email is required and should be at least 10 characters long."]
 
     },
     password: {
         type: String,
         required: true,
+        minLength: [4, "The password is required and should be at least"]
 
     }
 })
 
 
-// · The email is required and should be at least 10 characters long.
 
-// · The password is required and should be at least 4 characters long.
-
-// · The repeat password is required and should be equal to the password.
 const User = mongoose.model('User', userSchema)
 
+// userSchema.pre('save', function (next) {
+//     if (!this.username || !this.email || !this.password) {
+//         next(new Error('Name and email are required'));
+//     } else {
+//         next();
+//     }
+// });
 
 userSchema.pre('save', async function () {
     const hash = await bcrypt.hash(this.password, 12)
